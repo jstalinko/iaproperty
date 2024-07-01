0a6dce206d1f1031a1b7daa236d994be1248b99c
jstalinko <alinkodotid@gmail.com>
2024-07-01 21:37:11 +0700

    fix sosmed

--- resources/js/Components/Footer.vue
+++ resources/js/Components/Footer.vue
@@ -25,12 +25,7 @@
 				<nav>
 					<h5 class="font-bold text-2xl text-slate-500 mb-8">SOCIAL MEDIA</h5>
 					<div class="flex gap-5 mb-3">
-						<Link href="#" class="px-3 py-2 bg-blue-600 text-white rounded text-xl"><i class="mdi mdi-facebook"></i></Link>
-						<Link href="#" class="px-3 py-2 bg-pink-600 text-white rounded text-xl"><i class="mdi mdi-instagram"></i></Link>
-						<Link href="#" class="px-3 py-2 bg-gray-600 text-white rounded text-xl"><i class="mdi mdi-twitter"></i></Link>
-						<Link href="#" class="px-3 py-2 bg-green-600 text-white rounded text-xl"><i class="mdi mdi-whatsapp"></i></Link>
-
-
+						<Link  :class="classNames[sos.type]"  v-for="(sos,index) in Socmed" :href="sos.link"><i :class="icons[sos.type]"></i> </Link>
 					</div>
 					<p class="text-lg max-w-sm">
 						Jangan ragu untuk berkonsultasi dan tanya kami! 
@@ -49,6 +44,23 @@
 
 <script setup>
 import { Link } from '@inertiajs/vue3';
-
+import {ref} from 'vue';
 defineProps({Global: Object, Socmed: Object,Pages: Object});
+const icons = ref({
+	'tiktok':'mdi mdi-music-note-eighth',
+	'instagram':'mdi mdi-instagram',
+	'facebook':'mdi mdi-facebook',
+	'twitter':'mdi mdi-twitter',
+	'whatsapp':'mdi mdi-whatsapp',
+	'youtube':'mdi mdi-youtube'
+});
+const classNames = ref({
+	'tiktok':'px-3 py-2 bg-gray-600 text-white rounded text-xl hover:bg-gray-500',
+	'instagram':'px-3 py-2 bg-pink-600 text-white rounded text-xl hover:bg-pink-500',
+	'facebook':'px-3 py-2 bg-blue-600 text-white rounded text-xl hover:bg-blue-500',
+	'twitter':'px-3 py-2 bg-gray-600 text-white rounded text-xl hover:bg-gray-500',
+	'whatsapp':'px-3 py-2 bg-green-600 text-white rounded text-xl hover:bg-green-500',
+	'youtube':'px-3 py-2 bg-red-600 text-white rounded text-xl hover:bg-red-500'
+});
+
 </script>
